1ca5f01931e9bf315e46a4d786bb895d6c4d264c
Antonio013Kantun <119888477+Antonio013Kantun@users.noreply.github.com>
2024-04-13 21:08:15 -0500

    Vista de login y register

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+import LoginScreen from './src/screens/LoginScreen';
+import RegisterScreen from './src/screens/RegisterScreen';
+import { NavigationContainer } from '@react-navigation/native';
 
-export default function App() {
+
+
+const Stack = createStackNavigator();
+
+function App() {
   return (
-    <View style={styles.container}>
-      <Text>Prime park ddd</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+     <Stack.Navigator>
+  <Stack.Screen name="Login" component={LoginScreen} />
+  <Stack.Screen name="Register" component={RegisterScreen} />
+</Stack.Navigator>
+
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
---
+++ src/screens/RegisterScreen.js
@@ -0,0 +1,123 @@
+import React from 'react';
+import { View, StyleSheet, Text, TextInput, Button, TouchableOpacity } from 'react-native';
+import CheckBox from '@react-native-community/checkbox';
+
+const RegisterScreen = () => {
+  const [firstName, setFirstName] = React.useState('');
+  const [lastName, setLastName] = React.useState('');
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
+  const [confirmPassword, setConfirmPassword] = React.useState('');
+  const [agreeTerms, setAgreeTerms] = React.useState(false);
+
+  const handleRegister = () => {
+    // Aquí puedes agregar la lógica para registrar al usuario
+    console.log('Nombres:', firstName);
+    console.log('Apellidos:', lastName);
+    console.log('Correo:', email);
+    console.log('Contraseña:', password);
+    console.log('Confirmar Contraseña:', confirmPassword);
+    console.log('Aceptar Términos y Condiciones:', agreeTerms);
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.formContainer}>
+      <Text style={styles.label}>Nombres</Text>
+        <TextInput
+          placeholder="Diego"
+          value={firstName}
+          onChangeText={setFirstName}
+          style={styles.input}
+        />
+        <Text style={styles.label}>Apellidos</Text>
+        <TextInput
+          placeholder="Aleman Mena"
+          value={lastName}
+          onChangeText={setLastName}
+          style={styles.input}
+        />
+        <Text style={styles.label}>Correo</Text>
+        <TextInput
+          placeholder="diego@gmail.com"
+          value={email}
+          onChangeText={setEmail}
+          style={styles.input}
+        />
+        <Text style={styles.label}>Contraseña</Text>
+        <TextInput
+          placeholder="********"
+          value={password}
+          onChangeText={setPassword}
+          secureTextEntry
+          style={styles.input}
+        />
+        <Text style={styles.label}>Confirmar contraseña</Text>
+        <TextInput
+          placeholder="********"
+          value={confirmPassword}
+          onChangeText={setConfirmPassword}
+          secureTextEntry
+          style={styles.input}
+        />
+        {/* <View style={styles.checkboxContainer}>
+          <CheckBox
+            value={agreeTerms}
+            onValueChange={setAgreeTerms}
+            style={styles.checkbox}
+          />
+          <Text style={styles.label}>Aceptar Términos y Condiciones</Text>
+        </View> */}
+        <Button title="Registrar" onPress={handleRegister} style={styles.button} />
+        <TouchableOpacity>
+          <Text style={styles.link}>Ya tienes una cuenta? Inicia sesión</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  formContainer: {
+    width: '80%',
+  },
+  input: {
+    marginVertical: 5,
+    borderBottomWidth: 1,
+    borderBottomColor: '#CCCCCC',
+    paddingVertical: 2,
+    backgroundColor: 'white',
+  },
+  checkboxContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  checkbox: {
+    alignSelf: 'center',
+  },
+  label: {
+    color: '#9695A8',
+    fontFamily: 'sans-serif',
+    fontSize: 12,
+  },
+  button: {
+    marginVertical: 10,
+    backgroundColor: '#656CEE',
+    borderRadius: 5,
+  },
+  link: {
+    marginTop: 20,
+    color: 'blue',
+    textDecorationLine: 'underline',
+    textAlign: 'center',
+  },
+});
+
+export default RegisterScreen;
